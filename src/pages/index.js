9d1d02fa21bf2bfc20b0695f5f7753dc5bd8079f
cpinillam <carlos.pinilla.m@gmail.com>
2023-04-20 11:08:42 +0200

    logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,22 +19,9 @@ export default function Home() {
 
        
 
-       {/* <!-- Mobile menu button -->*/ }
-        <div class="flex lg:hidden">
-            <button  type="button" class="text-gray-500 hover:text-gray-600 focus:text-gray-600 focus:outline-none dark:text-gray-200 dark:hover:text-gray-400 dark:focus:text-gray-400" aria-label="toggle menu">
-            <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
-            </svg>
-
-            <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-            </button>
-        </div>
-        </div>
 
         
-        <img className='mix-blend-multiply w-60 translate-y-6' src='./img/logo-propuestas-tech.png' alt='logo factoria f5'/>
+        <img className='mix-blend-multiply max-w-60 translate-y-6' src='./img/logo-propuestas-tech.png' alt='logo factoria f5'/>
             
 
       { /* <!-- Mobile Menu open: "block", Menu closed: "hidden" -->*/}
